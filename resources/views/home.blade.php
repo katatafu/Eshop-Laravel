f22d43ffc7a2074c752bf9df8efacf02bf775dfa
ondre masny <icelyy@seznam.cz>
2024-12-03 08:14:07 +0100

    add contact page to navigation stack

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <!-- Hero Section with Image and Gradient -->
     <div class="relative h-screen">
-    <img src="https://imgs.search.brave.com/ko2Q7ZOGLlqvLvF_YRANMCfq0u33Z5RmFO9DCe4WCQw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cHJvZC53ZWJzaXRl/LWZpbGVzLmNvbS81/YTllZTY0MTZlOTBk/MjAwMDFiMjAwMzgv/NjI4OWYwYmZhOTIw/YTk1OGYyYjQxNmY3/X2JsYWNrLWdyYWRp/ZW50LnBuZw" alt="Vítejte" class="object-cover w-full h-3/4 z-10">
-    <div class="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50"></div>
-    <div class="flex items-start justify-center h-3/4 relative pt-12"> 
-        <h1 class="text-white text-5xl font-bold text-center z-10">Vítejte na naší stránce!</h1>
+        <img src="https://imgs.search.brave.com/ko2Q7ZOGLlqvLvF_YRANMCfq0u33Z5RmFO9DCe4WCQw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cHJvZC53ZWJzaXRl/LWZpbGVzLmNvbS81/YTllZTY0MTZlOTBk/MjAwMDFiMjAwMzgv/NjI4OWYwYmZhOTIw/YTk1OGYyYjQxNmY3/X2JsYWNrLWdyYWRp/ZW50LnBuZw" alt="Vítejte" class="object-cover w-full h-3/4">
+        <div class="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50"></div>
+        <div class="flex items-center justify-center h-3/4 relative">
+            <h1 class="text-white text-5xl font-bold text-center z-10">Vítejte na naší stránce!</h1>
+        </div>
     </div>
-</div>
 
     <!-- Why Choose Us Section -->
     <div class="py-10 text-center bg-gray-100">
@@ -61,7 +61,7 @@
 
     <!-- Contact Form Section -->
     <div class="py-10">
-        <h2 class="text-white text-3xl font-semibold text-center">Kontaktní formulář</h2>
+        <h2 class="text-3xl font-semibold text-center">Kontaktní formulář</h2>
         <form class="max-w-md mx-auto mt-4 p-6 bg-white shadow-md rounded-lg" action="#" method="POST">
             @csrf
             <div class="mb-4">
